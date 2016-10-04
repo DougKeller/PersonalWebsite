4d77ea839b437c40e9a1270689db2548b6a2f542
Doug Keller <dkeller@synergydatasystems.com>
2016-10-03 23:31:26 -0400

    Remove date

--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var setTime = function() {
-    var date = moment().format('dddd MMMM Do - h:mm:ss a').toLowerCase();
-    document.getElementById('date').innerHTML = date;
-  };
-
-  setInterval(setTime, 100);
-})();
